58a1e52eaf948dd48f41e60c160a3c0ce94903b0
SSEMWOGERERE948 <strevor948@gmail.com>
2024-10-30 08:29:24 +0300

    first commit

---
+++ src/main/java/com/cosek/edms/memo/MemoRepository.java
@@ -0,0 +1,9 @@
+package com.cosek.edms.memo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+}
